0ccde143842909c1c29a1bd818b6ab25a5a9a7e3
Irvin-Tandiarrang-Sumual <irvintandiarrang23@gmail.com>
2026-04-01 20:45:19 +0700

    feat(compiler) : writing error msg

--- src/Compiler.cpp
+++ src/Compiler.cpp
@@ -15,6 +15,14 @@ void Compiler::lexer() {
 
     std::vector<Token> tokens = lexing.getTokens();
 
+    // tulis error
+    std::cout << "\n\n";
+    for (size_t i = 0; i < lexing.getErrors().size(); i++) {
+        std::cout << lexing.getErrors().at(i);
+    }
+    std::cout << "\n\n";
+    
+
     std::cout << "Finish Tokenize\n";
     std::cout << "Going to write the result into result-tc-1.txt\n";
     Writer write("result-tc-1.txt", tokens);
