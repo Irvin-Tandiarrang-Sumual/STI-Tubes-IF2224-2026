8278f9ddfad2b9e3969fe19cbd628be9a213f32a
Irvin <irvintandiarrang23@gmail.com>
2026-04-02 23:00:40 +0700

    refactor : delete unused directory (already moved to writer)

--- src/output/OutputHandler.cpp
+++
@@ -1,8 +0,0 @@
-#include "OutputHandler.hpp"
-
-#include "Writer.hpp"
-
-void writeToFile(const std::string &filename, const std::vector<Token> &tokens) {
-    Writer writer(filename, tokens);
-    writer.writeToFile();
-}
