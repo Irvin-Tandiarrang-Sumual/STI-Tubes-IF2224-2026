24958a38efced11414f5319b85fc975600bc3a9a
Irvin-Tandiarrang-Sumual <irvintandiarrang23@gmail.com>
2026-05-04 19:47:54 +0700

    feat(Parser): implementing parsing related to program structure (program, programHeader, block)

---
+++ src/parser/rules/ProgramStructure.cpp
@@ -0,0 +1,37 @@
+#include "../Parser.hpp"
+/*
+Production Rule :
+    PROGRAM → PROGRAM-HEADER + DECLARATION-PART + COMPOUND-STATEMENT + period
+*/
+CSTNodes* Parser::parseProgram() {
+    CSTNodes* node = new CSTNodes(NonTerminal::PROGRAM, peek().codeLocation);
+    node->addChild(parseProgramHeader());
+    node->addChild(parseDeclarationPart());
+    node->addChild(parseCompoundStatement());
+    node->addChild(new CSTNodes(expect(TokenType::period)));
+
+    return node;
+}
+
+/*
+Production Rule :
+    PROGRAM-HEADER → programsy + ident + semicolon
+*/
+CSTNodes* Parser::parseProgramHeader() {
+    CSTNodes* node = new CSTNodes(NonTerminal::PROGRAM_HEADER, peek().codeLocation);
+    node->addChild(new CSTNodes(expect(TokenType::programsy)));
+    node->addChild(new CSTNodes(expect(TokenType::ident)));
+    node->addChild(new CSTNodes(expect(TokenType::semicolon)));
+    return node;
+}
+
+/*
+Production Rule :
+    BLOCK -> DECLARATION-PART + COMPOUND-STATEMENT
+*/
+CSTNodes* Parser::parseBlock() {
+    CSTNodes* node = new CSTNodes(NonTerminal::BLOCK, peek().codeLocation);
+    node->addChild(parseDeclarationPart());
+    node->addChild(parseCompoundStatement());
+    return node;
+}
