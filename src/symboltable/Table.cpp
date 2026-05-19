6a94fd5d34189a284d1eb6711654e9bf8c3f1e74
Irvin-Tandiarrang-Sumual <irvintandiarrang23@gmail.com>
2026-05-20 05:42:43 +0700

    feat: to string in table and entries

--- src/symboltable/Table.cpp
+++ src/symboltable/Table.cpp
@@ -9,3 +9,17 @@ template <typename T>
 T& Table<T>::get(int index) {
     return entries_.at(index);
 }
+template <typename T>
+std::string Table<T>::toString(const std::string& tableName) const {
+    std::stringstream ss;
+    ss << tableName << " :\n";
+    
+    ss << T::getHeader() << "\n";
+    
+    ss << std::string(60, '-') << "\n"; 
+    
+    for (size_t i = 0; i < entries_.size(); ++i) {
+        ss << entries_[i].toString() << "\n";
+    }
+    return ss.str();
+}
