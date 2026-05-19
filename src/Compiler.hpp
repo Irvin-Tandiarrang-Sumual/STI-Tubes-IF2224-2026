f027b65f3bddcd38c4fe19583c5e5eaf78b61ec7
Irvin-Tandiarrang-Sumual <irvintandiarrang23@gmail.com>
2026-05-19 20:08:17 +0700

    feat: dummy compiler for semantic

--- src/Compiler.hpp
+++ src/Compiler.hpp
@@ -1,22 +1,27 @@
 #pragma once
-
+#include <iostream>
 #include <filesystem>
 #include <vector>
 
 #include "lexer/Lexer.hpp"
 #include "parser/Parser.hpp"
 #include "writer/Writer.hpp"
-
+#include "ast/ASTBuilder.hpp"
+#include "ast/ASTNodes.hpp"
 class Compiler {
-public:
-    explicit Compiler(const std::filesystem::path &path, const std::string &outputDir);
-    ~Compiler();
+    public:
+        explicit Compiler(const std::filesystem::path &path, const std::string &outputDir);
+        ~Compiler();
 
-    void lexer();
-    void parser();
+        void lexer();
+        void parser();
+        void semantic();
 
-private:
-    std::filesystem::path inputPath;
-    std::string outputDir;
-    std::vector<Token> tokens_;
+    private:
+        std::filesystem::path inputPath;
+        std::string outputDir;
+        std::vector<Token> tokens_;
+        //
+        CSTNodes* cstRoot_;
+        std::vector<std::string> cstErrors_;
 };
